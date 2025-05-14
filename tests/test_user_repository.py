19f85685b7889cb0dbe9e735a6966f6779f36ee0
ianmacdonnacha <91433149+IanMacDonnacha@users.noreply.github.com>
2025-05-14 10:01:06 +0100

    Update test cases in test_user_repo to align with additonal inserts to seed file

--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -9,7 +9,9 @@ def test_all(db_connection):
     users = repository.all()
 
     assert users == [
-    User(1, 'John Doe', 'johndoe@bing.com' , '1234567890', 'hashed_password_1')
+    User(1, 'John Doe', 'johndoe@bing.com' , '1234567890', 'hashed_password_1'),
+    User(2, 'Alice Smith', 'alice@example.com', '0987654321', 'hashed_password_2'),
+    User(3, 'Bob Johnson', 'bob.j@staymail.com', '07700900000', 'hashed_password_3')
     ]
 
 def test_find(db_connection):
@@ -23,13 +25,15 @@ def test_create(db_connection):
     db_connection.seed("seeds/bnb_seed.sql")
     repository = UserRepository(db_connection)
 
-    created_user = repository.create(User(2, 'Jane Doe', 'janedoe@bing.com' , '987654321', 'hashed_password_2'))
-    assert created_user == User(2, 'Jane Doe', 'janedoe@bing.com' , '987654321', 'hashed_password_2')
+    created_user = repository.create(User(4, 'Jane Doe', 'janedoe@bing.com' , '987654321', 'hashed_password_4'))
+    assert created_user == User(4, 'Jane Doe', 'janedoe@bing.com' , '987654321', 'hashed_password_4')
 
     result = repository.all()
     assert result == [
         User(1, 'John Doe', 'johndoe@bing.com' , '1234567890', 'hashed_password_1'),
-        User(2, 'Jane Doe', 'janedoe@bing.com' , '987654321', 'hashed_password_2')
+        User(2, 'Alice Smith', 'alice@example.com', '0987654321', 'hashed_password_2'),
+        User(3, 'Bob Johnson', 'bob.j@staymail.com', '07700900000', 'hashed_password_3'),
+        User(4, 'Jane Doe', 'janedoe@bing.com' , '987654321', 'hashed_password_4')
     ]
 
 def test_delete(db_connection):
@@ -38,7 +42,10 @@ def test_delete(db_connection):
     repository.delete(1)
 
     result = repository.all()
-    assert result == []
+    assert result == [
+        User(2, 'Alice Smith', 'alice@example.com', '0987654321', 'hashed_password_2'),
+        User(3, 'Bob Johnson', 'bob.j@staymail.com', '07700900000', 'hashed_password_3')
+        ]
 
 
 def test_update(db_connection):
@@ -50,4 +57,8 @@ def test_update(db_connection):
 
     result = repository.all()
     print(result)
-    assert result == [User(1, 'Bob Doe', 'bobdoe@bing.com' , '987654321', 'hashed_password_2')]
+    assert result == [
+        User(2, 'Alice Smith', 'alice@example.com', '0987654321', 'hashed_password_2'),
+        User(3, 'Bob Johnson', 'bob.j@staymail.com', '07700900000', 'hashed_password_3'),
+        User(1, 'Bob Doe', 'bobdoe@bing.com' , '987654321', 'hashed_password_2')
+        ]
