3d443b2b3a0999024e892149f5265f383d624c24
Giuliano Paolini <giulianopaolini91@gmail.com>
2025-05-15 14:52:04 +0100

    property_request_form_&_corresponding_route

--- app.py
+++ app.py
@@ -1,13 +1,54 @@
 import os
 from flask import Flask, request, redirect, render_template, abort, url_for, flash
+from flask_mail import Mail, Message
 from lib.database_connection import get_flask_database_connection
 from lib.property_repository import PropertyRepository
 from lib.property import Property
 
 # Create a new Flask app
 app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'giulianopaolini1991@gmail.com'
+app.config['MAIL_PASSWORD'] = 'ioyisepfpeakqvci'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.secret_key = '02z[^=~3F(qL'
+
+mail = Mail(app)
+
 # == Your Routes Here ==
+
+# MAIL APP
+@app.route('/property_request', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        from_date = request.form['from_date']
+        to_date = request.form['to_date']
+
+        if not name or not email or not message:
+            flash('All fields are required.', 'warning')
+            return redirect('/property_request')
+
+        try:
+            msg = Message('Contact Form Submission', 
+                sender='giulianopaolini1991@gmail.com',  
+                recipients=['giulianopaolini1991@gmail.com'])
+            msg.body = f"Name: {name}\nUser Email: {email}\nFrom: {from_date}\nTo: {to_date}\n\nMessage:\n{message}"
+            mail.send(msg)
+            flash('Message sent successfully!', 'success')
+        except Exception as e:
+            flash(f'Failed to send message. Error: {str(e)}', 'danger')
+        return redirect('/property_request')
+
+    return render_template('property_request.html')
+
+# END MAIL APP
+
+
 @app.route('/<int:property_id>')
 def show_property(property_id):
     connection = get_flask_database_connection(app)
@@ -22,7 +63,7 @@ def show_property(property_id):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    from lib.password_manager import PasswordAuthenticator
+    from lib.password_authenticator import PasswordAuthenticator
     if request.method == 'POST':
         connection = get_flask_database_connection(app)
         email = request.form['email']
